745371c4ca4c83109a732c2e6117f55b7d4e211b
Renzo <larenzostclair@gmail.com>
2022-11-17 19:19:14 -0600

    test

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 // Write your JavaScript code here.
-// Remember to pay attention to page loading!
+// Remember to pay attention to page loading
+window.addEventListener("load", function(){
+    const takeoff = document.getElementById('takeoff')
+     flightStatus = document.getElementById("flightStatus")
+    takeoff.addEventListener('click', function() {
+        let takeoffConfirm = window.confirm('confirm that the shuttle is ready for takeoff.')
+        if(takeoffConfirm){
+            document.flightStatus.innerHTML = "Shuttle in flight"
+        }
+    })
+})
